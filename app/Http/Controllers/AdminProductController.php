d2f73bc92c2504db43195247e548c4eb056a12d2
electricpizzza <zakariae.dinar@gmail.com>
2020-05-03 02:37:55 +0000

    nothing

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -128,8 +128,7 @@ class AdminProductController extends Controller
 
     public function update(Product $product)
     {
-        $data = request()->all();
-/*
+
         $data = request()->validate([
              "title" => "required|string",
              "category" => "required|string",
@@ -147,7 +146,6 @@ class AdminProductController extends Controller
              "image2" => "image",
              "image3" => "image",
         ]);
-        */
         $tags = explode(",",$data["tag"]);
 
         $product->update([
