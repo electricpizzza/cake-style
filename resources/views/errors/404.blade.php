f9d8eff01def6f57fc25eb0424bb5eaadf09b617
electricpizzza <zakariae.dinar@gmail.com>
2020-05-04 15:32:56 +0000

    product Images

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
         <div class="row h-100 align-items-center">
             <div class="col-12">
                 <div class="page-title text-center">
-                    <h2>Page Not Found - 404</h2>
+                    <h2>Page Not Found - 404 :( </h2>
                 </div>
             </div>
         </div>
@@ -19,8 +19,6 @@
                 <div class='regular-page-content-wrapper section-padding-80'>
                     <div class='regular-page-text'>
                         <h2>What you are looking for is not available.</h2>
-                        <p>Mauris viverra cursus ante laoreet eleifend. Donec vel fringilla ante. Aenean finibus velit id urna vehicula, nec maximus est sollicitudin. Praesent at tempus lectus, eleifend blandit felis. Fusce augue arcu, consequat a nisl aliquet, consectetur elementum turpis. Donec iaculis lobortis nisl, et viverra risus imperdiet eu. Etiam mollis posuere elit non sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis arcu a magna sodales venenatis. Integer non diam sit amet magna luctus mollis ac eu nisi. In accumsan tellus ut dapibus blandit.</p>
-
                         <blockquote>
                             <h6><i class='fa fa-quote-left' aria-hidden='true'></i> Sorry we coulden't find what you are looking maybe next time.</h6>
                             <div class="checkout-btn mt-100">
