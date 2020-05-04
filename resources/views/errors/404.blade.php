22a0d60d4d335289b7680a9c918385a5f62bdc0e
electricpizzza <zakariae.dinar@gmail.com>
2020-05-04 15:13:42 +0000

    product Images

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="breadcumb_area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12">
+                <div class="page-title text-center">
+                    <h2>Page Not Found - 404</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div class='container'>
+        <div class='row justify-content-center'>
+            <div class='col-12 col-md-8'>
+                <div class='regular-page-content-wrapper section-padding-80'>
+                    <div class='regular-page-text'>
+                        <h2>What you are looking for is not available.</h2>
+                        <p>Mauris viverra cursus ante laoreet eleifend. Donec vel fringilla ante. Aenean finibus velit id urna vehicula, nec maximus est sollicitudin. Praesent at tempus lectus, eleifend blandit felis. Fusce augue arcu, consequat a nisl aliquet, consectetur elementum turpis. Donec iaculis lobortis nisl, et viverra risus imperdiet eu. Etiam mollis posuere elit non sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis arcu a magna sodales venenatis. Integer non diam sit amet magna luctus mollis ac eu nisi. In accumsan tellus ut dapibus blandit.</p>
+
+                        <blockquote>
+                            <h6><i class='fa fa-quote-left' aria-hidden='true'></i> Sorry we coulden't find what you are looking maybe next time.</h6>
+                            <div class="checkout-btn mt-100">
+                                <a href="/" class="btn essence-btn">Start Shopping</a>
+                            </div>
+                        </blockquote>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
